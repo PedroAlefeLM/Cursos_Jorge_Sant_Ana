e8634222c970297d3b29e652fc139a007ffbd6ed
Xhampion <148000582+PedroAlefeLM@users.noreply.github.com>
2024-03-13 18:34:10 -0300

    App Super Gestão

--- 01_Laravel/App_Super_Gestao/routes/web.php
+++ 01_Laravel/App_Super_Gestao/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return 'Olá, seja bem vindo ao curso';
+// });
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'SobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'Contato']);
