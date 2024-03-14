65e73970e252cb73aceab42c0f76a6e7d66e5762
Xhampion <148000582+PedroAlefeLM@users.noreply.github.com>
2024-03-14 17:21:45 -0300

    App Super Gestão

--- 01_Laravel/App_Super_Gestao/routes/web.php
+++ 01_Laravel/App_Super_Gestao/routes/web.php
@@ -6,8 +6,31 @@ use Illuminate\Support\Facades\Route;
 //     return 'Olá, seja bem vindo ao curso';
 // });
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'SobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'SobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'Contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'Contato'])->name('site.contato');
+
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('site.rota1');
+
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para a página inicial';
+});
