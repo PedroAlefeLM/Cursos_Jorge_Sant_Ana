0302b6d95b99728eb482c8ff6e36eafd51699b27
Xhampion <148000582+PedroAlefeLM@users.noreply.github.com>
2024-03-18 11:01:45 -0300

    App Super Gestão

--- 01_Laravel/App_Super_Gestao/resources/views/app/Fornecedor/index.blade.php
+++ 01_Laravel/App_Super_Gestao/resources/views/app/Fornecedor/index.blade.php
@@ -6,15 +6,26 @@
 @endphp
 
 @isset($fornecedores)
-    @forelse ( $fornecedores as $indice => $fornecedor )
-        
-        Fornecedor : @{{ $fornecedor['nome'] }}
+    @forelse ( $fornecedores as $indice => $fornecedor ) 
+        Iteração atual: {{ $loop->iteration }}
         <br/>
-        Status: @{{ $fornecedor['status']}}
+        Fornecedor : {{ $fornecedor['nome'] }}
         <br/>
-        CNPJ: @{{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}
+        Status: {{ $fornecedor['status']}}
         <br/>
-        Telefone: (@{{ $fornecedor['ddd'] ?? '' }}) {@{ $fornecedor['telefone'] ?? '' }}
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}
+        <br/>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if($loop->first)
+            Primeira iteração do Loop
+            <br>
+            Total de registros: {{ $loop->count}}
+
+        @endif
+        @if($loop->last)
+            Ultima iteração do Loop
+        @endif
         <hr>
     @empty 
         Não existem fornecedores cadastrados!!!
