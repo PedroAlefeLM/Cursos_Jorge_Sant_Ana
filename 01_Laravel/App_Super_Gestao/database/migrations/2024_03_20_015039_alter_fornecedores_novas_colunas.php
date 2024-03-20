645ff58ebd6ad2671344111073f0ddd805bb8b59
Xhampion <148000582+PedroAlefeLM@users.noreply.github.com>
2024-03-20 17:54:18 -0300

    App Super Gestão

--- 01_Laravel/App_Super_Gestao/database/migrations/2024_03_20_015039_alter_fornecedores_novas_colunas.php
+++ 01_Laravel/App_Super_Gestao/database/migrations/2024_03_20_015039_alter_fornecedores_novas_colunas.php
@@ -22,6 +22,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('fornecedores', function (Blueprint $table) {
+            // para remover colunas
+            // $table->dropColumn('uf');
+            // $table->dropColumn('email');
+            $table->dropColumn(['uf', 'email']);
+        });
     }
 };
